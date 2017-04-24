148dd726baea849032039410a447b715ddb54b5c
ClaytonTaylor <claytaylor@cox.net>
2017-04-24 12:01:05 -0500

    Updated with Bootstrap

---
+++ directory/main.js
@@ -0,0 +1,54 @@
+console.log("Main JS is loaded")
+
+// angular.module('personalWebsite', ['ui.router'])
+//   .config(Config)
+//
+//
+//   function Config($stateProvider, $urlRouterProvider) {
+//       // $urlRouterProvider.otherwise('/Login');
+//       $stateProvider
+//
+//       // .state('Home', {
+//       //     url: '/home',
+//       //     templateUrl: './views/home.html'
+//       //     // controller: '',
+//       //     // authenticate: true
+//       // })
+//       .state('Profile', {
+//           url: '/profile',
+//           templateUrl: './views/profile.html'
+//           // controller: '',
+//           // authenticate: true
+//       })
+//         .state('Experience', {
+//           url: '/experience',
+//           templateUrl: './views/experience.html'
+//           // controller: '',
+//           // authenticate: true
+//       })
+//         .state('Technology', {
+//           url: '/technology',
+//           templateUrl: './views/technology.html'
+//           // controller: '',
+//           // authenticate: true
+//       })
+//         .state('Education', {
+//           url: '/education',
+//           templateUrl: './views/education.html'
+//           // controller: '',
+//           // authenticate: true
+//       })
+//         .state('Projects', {
+//           url: '/projects',
+//           templateUrl: './views/projects.html'
+//           // controller: '',
+//           // authenticate: true
+//       })
+//         .state('Photography', {
+//           url: '/photography',
+//           templateUrl: './views/photography.html'
+//           // controller: '',
+//           // authenticate: true
+//       })
+//      $urlRouterProvider.otherwise('/profile');
+//    }
